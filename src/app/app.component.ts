5da5b0fbb25bda68d55a0bd712bbea52a5275bd3
Keegan McCafferty <keegan.mccafferty@hyland.com>
2021-06-24 12:22:57 -0400

    feat: implement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as Flickity from 'flickity';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'flickity-issue';
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    var flkty = new Flickity('.carousel', {
+      cellSelector: '.carousel-cell',
+      setGallerySize: false,
+      adaptiveHeight: true,
+      cellAlign: 'left',
+      wrapAround: true,
+      autoPlay: 5000,
+      pauseAutoPlayOnHover: false,
+      draggable: false,
+    });
+  }
 }
